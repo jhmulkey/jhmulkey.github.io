4e33c6f032552f930411248bd77d75da12a6c6e2
jhmulkey <32462749+jhmulkey@users.noreply.github.com>
2017-11-13 15:21:10 -0500

    edit

---
+++ test-sites/random-drawing/main.js
@@ -0,0 +1,41 @@
+let students = [
+    "Aeralind Aldrich",
+    "Bronwyn Aldrich",
+    "Evan Brown",
+    "Samuel Collins",
+    "Ashton Cruice",
+    "Raegan Deming",
+    "Bella Mair",
+    "Connell Morris",
+    "Katie Neal",
+    "Annaliese Nelson",
+    "Hailey Norton",
+    "Jill Pahnke",
+    "Job Peoples",
+    "Blake Pidcock",
+    "Cameron Rogers",
+    "Liam Snyder",
+    "Macie Walker",
+    "Olivia Waters",
+];
+
+let pickedStudents = [];
+
+function chooseWinner() {
+    let i = Math.floor(Math.random() * (students.length - 1));
+    let winner = students[i];
+    if (winner === undefined) {
+        document.getElementById("winner").classList.add("white");
+        document.getElementById("winner").innerHTML = "NO MORE NAMES!";
+    } else {
+        document.getElementById("winner").innerHTML = winner;
+        students.splice(i, 1);
+        pickedStudents.push(winner);
+        document.getElementById("picked-students").innerHTML = "Picked: " + pickedStudents.join(", ");
+        document.getElementById("reset-button").classList.remove("hide");
+    };
+}
+
+function resetPage() {
+    location.reload();
+}
