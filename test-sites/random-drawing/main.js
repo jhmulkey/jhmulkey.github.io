d0ea778a5ad20de6e01ba9016f774b37fc0e59cc
jhmulkey <32462749+jhmulkey@users.noreply.github.com>
2017-11-18 12:52:38 -0500

    edit

--- test-sites/random-drawing/main.js
+++ test-sites/random-drawing/main.js
@@ -21,40 +21,20 @@ let students = [
 
 let pickedStudents = [];
 
-/*
 function chooseWinner() {
     let i = Math.floor(Math.random() * (students.length - 1));
     let winner = students[i];
-    if (winner === undefined) {
-        document.getElementById("winner").classList.add("white");
-        document.getElementById("winner").innerHTML = "No More Names!";
-    } else {
+    if (students.length > 0) {
         document.getElementById("winner").innerHTML = winner;
         students.splice(i, 1);
         pickedStudents.push(winner);
-        document.getElementById("picked-students").innerHTML = "Picked: " + pickedStudents.join(", ");
-        document.getElementById("reset-button").classList.remove("hide");
-    };
-}
-*/
-
-function chooseWinner() {
-    let i = Math.floor(Math.random() * (students.length - 1));
-    let winner = students[i];
-    if (winner === undefined) {
-        document.getElementById("winner").classList.add("white");
-        document.getElementById("winner").innerHTML = "No More Names!";
-    } else if (pickedStudents.length > 0) {
         document.getElementById("picked-students").classList.remove("hide");
-        document.getElementById("winner").innerHTML = winner;
-        students.splice(i, 1);
-        pickedStudents.push(winner);
-        document.getElementById("picked-students").innerHTML = "Picked: " + pickedStudents.join(", ");
+        document.getElementById("picked-students").innerHTML = '<span style="background-color: rgba(255,255,255,0.35)">Picked (' + pickedStudents.length + " of " + (pickedStudents.length + students.length) + ")</span><br/>" + pickedStudents.join("<br/>");
     } else {
-        document.getElementById("winner").innerHTML = winner;
-        students.splice(i, 1);
-        pickedStudents.push(winner);
-        document.getElementById("picked-students").innerHTML = "Picked: " + pickedStudents.join(", ");
+        document.getElementById("winner").classList.add("white");
+        document.getElementById("winner").innerHTML = "No More Names!";
+        document.getElementById("winner-button").classList.add("hide");
+        document.getElementById("reset-button").classList.remove("hide");
     };
 }
 
