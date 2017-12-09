18e6b52724d89b54336c7b025980a21fa9ff416d
jhmulkey <32462749+jhmulkey@users.noreply.github.com>
2017-12-09 18:19:32 -0500

    edit

---
+++ test-sites/dice-roll/main.js
@@ -0,0 +1,114 @@
+var diceRoll = new Audio();
+diceRoll.src = "dice-roll.mp3";
+
+var click = new Audio();
+click.src = "click.mp3";
+
+whiteArray = [
+    "url(images/white-dice-1.png)",
+    "url(images/white-dice-2.png)",
+    "url(images/white-dice-3.png)",
+    "url(images/white-dice-4.png)",
+    "url(images/white-dice-5.png)",
+    "url(images/white-dice-6.png)"
+];
+
+blackArray = [
+    "url(images/black-dice-1.png)",
+    "url(images/black-dice-2.png)",
+    "url(images/black-dice-3.png)",
+    "url(images/black-dice-4.png)",
+    "url(images/black-dice-5.png)",
+    "url(images/black-dice-6.png)"
+];
+
+redArray = [
+    "url(images/red-dice-1.png)",
+    "url(images/red-dice-2.png)",
+    "url(images/red-dice-3.png)",
+    "url(images/red-dice-4.png)",
+    "url(images/red-dice-5.png)",
+    "url(images/red-dice-6.png)"
+];
+
+greenArray = [
+    "url(images/green-dice-1.png)",
+    "url(images/green-dice-2.png)",
+    "url(images/green-dice-3.png)",
+    "url(images/green-dice-4.png)",
+    "url(images/green-dice-5.png)",
+    "url(images/green-dice-6.png)"
+];
+
+blueArray = [
+    "url(images/blue-dice-1.png)",
+    "url(images/blue-dice-2.png)",
+    "url(images/blue-dice-3.png)",
+    "url(images/blue-dice-4.png)",
+    "url(images/blue-dice-5.png)",
+    "url(images/blue-dice-6.png)"
+];
+
+function rollWhiteDice() {
+    diceRoll.play();
+    x = whiteArray[(Math.floor(Math.random() * 6))];
+    document.getElementById("roll-1-div").style.backgroundImage = x;
+};
+
+function rollBlackDice() {
+    diceRoll.play();
+    x = blackArray[(Math.floor(Math.random() * 6))];
+    y = blackArray[(Math.floor(Math.random() * 6))];
+    document.getElementById("roll-1-div").style.visibility = "visible";
+    document.getElementById("roll-2-div").style.visibility = "visible";
+    document.getElementById("roll-1-div").style.backgroundImage = x;
+    document.getElementById("roll-2-div").style.backgroundImage = y;
+};
+
+function rollRedDice() {
+    diceRoll.play();
+    x = redArray[(Math.floor(Math.random() * 6))];
+    y = redArray[(Math.floor(Math.random() * 6))];
+    document.getElementById("roll-1-div").style.visibility = "visible";
+    document.getElementById("roll-2-div").style.visibility = "visible";
+    document.getElementById("roll-1-div").style.backgroundImage = x;
+    document.getElementById("roll-2-div").style.backgroundImage = y;
+};
+
+function rollGreenDice() {
+    diceRoll.play();
+    x = greenArray[(Math.floor(Math.random() * 6))];
+    y = greenArray[(Math.floor(Math.random() * 6))];
+    document.getElementById("roll-1-div").style.visibility = "visible";
+    document.getElementById("roll-2-div").style.visibility = "visible";
+    document.getElementById("roll-1-div").style.backgroundImage = x;
+    document.getElementById("roll-2-div").style.backgroundImage = y;
+};
+
+function rollBlueDice() {
+    diceRoll.play();
+    x = blueArray[(Math.floor(Math.random() * 6))];
+    y = blueArray[(Math.floor(Math.random() * 6))];
+    document.getElementById("roll-1-div").style.visibility = "visible";
+    document.getElementById("roll-2-div").style.visibility = "visible";
+    document.getElementById("roll-1-div").style.backgroundImage = x;
+    document.getElementById("roll-2-div").style.backgroundImage = y;
+};
+
+function hide1() {
+    if (document.getElementById("roll-1-div").style.visibility == "visible") {
+        click.play();
+        document.getElementById("roll-1-div").style.visibility = "hidden";
+    } else {
+        document.getElementById("roll-1-div").style.visibility = "visible";
+    }
+}
+
+function hide2() {
+    if (document.getElementById("roll-2-div").style.visibility == "visible") {
+        click.play();
+        document.getElementById("roll-2-div").style.visibility = "hidden";
+    } else {
+        document.getElementById("roll-2-div").style.visibility = "visible";
+    }
+}
