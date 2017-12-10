8eb0b9b6e7ef080e1775f9846a1aeeb44002d59d
jhmulkey <32462749+jhmulkey@users.noreply.github.com>
2017-12-10 15:16:29 -0500

    edit

--- test-sites/dice-roll/main.js
+++ test-sites/dice-roll/main.js
@@ -123,3 +123,79 @@ function whiteDiceVisible() {
         document.getElementById("roll-3-div").style.visibility = "hidden";
     }
 }
+
+function tile1() {
+    alert("more than 1 of each building type allowed per city");
+}
+
+function tile2() {
+    alert("1 worker earned for each mine at end of each phase in addition to the silverling");
+}
+
+function tile3() {
+    alert("2 silverlings per goods sale, not just 1");
+}
+
+function tile4() {
+    alert("1 worker earned per goods sale in addition to the silverling(s)");
+}
+
+function tile5() {
+    alert("receive goods from 2 neighboring depots (not just 1) when ship placed");
+}
+
+function tile6() {
+    alert("silverlings may be used to buy tiles from any depot, not just the black depot");
+}
+
+function tile7() {
+    alert("if you place an animal tile when knowledge tile #7 is on estate, add 1 extra point for the animal tile itself that you place plus 1 extra point for any other animal tiles of the same animal type ");
+}
+
+function tile8() {
+    alert("worker tiles can adjust dice roll by up to +2 or -2");
+}
+
+function tile9() {
+    alert("any dice result may be adjusted +1 or -1 to place a building");
+}
+
+function tile10() {
+    alert("any dice result may be adjusted +1 or -1 to place a ship or animal tile");
+}
+
+function tile11() {
+    alert("any dice result may be adjusted +1 or -1 to place a castle, knowledge tile, or mine");
+}
+
+function tile12() {
+    alert("any dice result may be adjusted +1 or -1 to acquire any new tile");
+}
+
+function tile13() {
+    alert("when a dice is traded for worker tiles, a silverling is also earned");
+}
+
+function tile14() {
+    alert("4 worker tiles traded for a dice, not just 2");
+}
+
+function tile15() {
+    alert("at end of game: 3 points for every goods type sold");
+}
+
+function tile16_23() {
+    alert("at end of game: 4 points for each corresponding building on the estate");
+}
+
+function tile24() {
+    alert("at end of game: 4 points for each animal type on estate");
+}
+
+function tile25() {
+    alert("at end of game: 1 point for every goods tile sold");
+}
+
+function tile26() {
+    alert("at end of game: 2 points for each bonus tile");
+}
