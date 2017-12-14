f15bcce12305c6f4b0bdfb0a8fee2cebe3a8b8a6
jhmulkey <32462749+jhmulkey@users.noreply.github.com>
2017-12-14 16:00:01 -0500

    edit

--- test-sites/dice-roll/index.js
+++ test-sites/dice-roll/index.js
@@ -13,5 +13,19 @@ whiteArray = [
 function rollWhiteDice() {
     diceRoll.play();
     x = whiteArray[(Math.floor(Math.random() * 6))];
+    y = Math.floor(Math.random() * 8 + 2);
     document.getElementById("roll-1-div").style.backgroundImage = x;
+    document.getElementById("board-div").style.visibility = "visible";
+    document.getElementById("board-num").innerHTML = y;
 };
+
+function reRoll() {
+    diceRoll.play();
+    x = whiteArray[(Math.floor(Math.random() * 6))];
+    document.getElementById("roll-1-div").style.backgroundImage = x;
+}
+
+function reChoose() {
+    y = Math.floor(Math.random() * 8 + 2);
+    document.getElementById("board-num").innerHTML = y;
+}
