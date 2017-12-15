1a12f30b857dba459dc8490cf73de2b8f214d578
jhmulkey <32462749+jhmulkey@users.noreply.github.com>
2017-12-14 23:25:31 -0500

    edit

--- test-sites/dice-roll/bonus.js
+++ test-sites/dice-roll/bonus.js
@@ -2,19 +2,19 @@ bonusArray = [
     "Up to 4 six-sided tiles may be stored on player board",
     "Sell all your goods",
     "Instantly move to first player position on turn order track",
-    "Take any building tile from the board (including black market)",
-    "Take any knowledge tile from the board (including black market)",
-    "Take any animal tile from the board (including black market)",
-    "Take a castle tile from the board (including black market)",
-    "Take a mine tile from the board (including black market)",
-    "Take a ship tile from the board (including black market)",
+    "Take any building tile from the board including black market",
+    "Take any knowledge tile from the board including black market",
+    "Take any animal tile from the board including black market",
+    "Take a castle tile from the board including black market",
+    "Take a mine tile from the board including black market",
+    "Take a ship tile from the board including black market",
     "You get one free move",
     "Take two silverlings",
     "Take two worker tiles",
     "Earn 2 silverlings for every mine on estate at end of each phase",
     "You earn 5 victory points",
-    "Each animal tile you place is worth double (but not previously placed tiles of same type on same pasture)",
-    "Place any stored six-sided tile onto estate, even if not adjacent to a previously placed tile"
+    "Each animal tile you place is worth double victory points but only at time of placement",
+    "Place any stored six-sided tile onto estate even if not adjacent to a previously placed tile"
 ];
 
 function chooseBonus() {
