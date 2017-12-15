d0a7bb3dffec0bf8d2e66a264d528eb86ff4b226
jhmulkey <32462749+jhmulkey@users.noreply.github.com>
2017-12-15 12:04:19 -0500

    edit

--- test-sites/dice-roll/index.js
+++ test-sites/dice-roll/index.js
@@ -23,9 +23,9 @@ function reRoll() {
     diceRoll.play();
     x = whiteArray[(Math.floor(Math.random() * 6))];
     document.getElementById("roll-1-div").style.backgroundImage = x;
-}
+};
 
 function reChoose() {
     y = Math.floor(Math.random() * 8 + 2);
     document.getElementById("board-num").innerHTML = y;
-}
+};
--- test-sites/dice-roll/main.js
+++ test-sites/dice-roll/main.js
@@ -68,7 +68,7 @@ function rollBlackDice() {
     document.getElementById("roll-3-div").style.backgroundImage = z;
     if (b == 6) {
         document.getElementById("bonus").style.visibility="visible";
-        bonusSound.play()
+        bonusSound.play();
     };
 };
 
@@ -85,7 +85,7 @@ function rollRedDice() {
     document.getElementById("roll-3-div").style.backgroundImage = z;
     if (b == 6) {
         document.getElementById("bonus").style.visibility="visible";
-        bonusSound.play()
+        bonusSound.play();
     };
 };
 
@@ -102,7 +102,7 @@ function rollGreenDice() {
     document.getElementById("roll-3-div").style.backgroundImage = z;
     if (b == 6) {
         document.getElementById("bonus").style.visibility="visible";
-        bonusSound.play()
+        bonusSound.play();
     };
 };
 
@@ -119,7 +119,7 @@ function rollBlueDice() {
     document.getElementById("roll-3-div").style.backgroundImage = z;
     if (b == 6) {
         document.getElementById("bonus").style.visibility="visible";
-        bonusSound.play()
+        bonusSound.play();
     };
 };
 
@@ -128,108 +128,108 @@ function hide1() {
         document.getElementById("roll-1-div").style.visibility = "hidden";
     } else {
         document.getElementById("roll-1-div").style.visibility = "visible";
-    }
-}
+    };
+};
 
 function hide2() {
     if (document.getElementById("roll-2-div").style.visibility == "visible") {
         document.getElementById("roll-2-div").style.visibility = "hidden";
     } else {
         document.getElementById("roll-2-div").style.visibility = "visible";
-    }
-}
+    };
+};
 
 function whiteDiceVisible() {
     if (document.getElementById("roll-3-div").style.visibility == "hidden") {
         document.getElementById("roll-3-div").style.visibility = "visible";
     } else {
         document.getElementById("roll-3-div").style.visibility = "hidden";
-    }
-}
+    };
+};
 
 function bonusButtonAppear() {
     x = Math.floor(Math.random() * 101);
     if (x == 50) {
         document.getElementById("bonus").style.visibility="visible";
-    }
-}
+    };
+};
 
 function bonusButtonDisappear() {
     document.getElementById("bonus").style.visibility="hidden";
-}
+};
 
 function tile1() {
     alert("more than 1 of each building type allowed per city");
-}
+};
 
 function tile2() {
     alert("1 worker earned for each mine at end of each phase in addition to the silverling");
-}
+};
 
 function tile3() {
     alert("2 silverlings per goods sale, not just 1");
-}
+};
 
 function tile4() {
     alert("1 worker earned per goods sale in addition to the silverling(s)");
-}
+};
 
 function tile5() {
     alert("receive goods from 2 neighboring depots (not just 1) when ship placed");
-}
+};
 
 function tile6() {
     alert("silverlings may be used to buy tiles from any depot, not just the black depot");
-}
+};
 
 function tile7() {
     alert("if you place an animal tile when knowledge tile #7 is on estate, add 1 extra point for the animal tile itself that you place plus 1 extra point for any other animal tiles of the same animal type on the same pasture");
-}
+};
 
 function tile8() {
     alert("worker tiles can adjust dice roll by up to +2 or -2");
-}
+};
 
 function tile9() {
     alert("any dice result may be adjusted +1 or -1 to place a building");
-}
+};
 
 function tile10() {
     alert("any dice result may be adjusted +1 or -1 to place a ship or animal tile");
-}
+};
 
 function tile11() {
     alert("any dice result may be adjusted +1 or -1 to place a castle, knowledge tile, or mine");
-}
+};
 
 function tile12() {
     alert("any dice result may be adjusted +1 or -1 to acquire any new tile");
-}
+};
 
 function tile13() {
     alert("when a dice is traded for worker tiles, a silverling is also earned");
-}
+};
 
 function tile14() {
     alert("4 worker tiles traded for a dice, not just 2");
-}
+};
 
 function tile15() {
     alert("at end of game: 3 points for every goods type sold");
-}
+};
 
 function tile16_23() {
     alert("at end of game: 4 points for each corresponding building on the estate");
-}
+};
 
 function tile24() {
     alert("at end of game: 4 points for each animal type on estate");
-}
+};
 
 function tile25() {
     alert("at end of game: 1 point for every goods tile sold");
-}
+};
 
 function tile26() {
     alert("at end of game: 2 points for each bonus tile");
-}
+};
