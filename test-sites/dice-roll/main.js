a5cd1bac65e8ebf528478c8c882ecb378fff08cf
jhmulkey <32462749+jhmulkey@users.noreply.github.com>
2017-12-20 22:19:51 -0500

    edit

--- test-sites/dice-roll/main.js
+++ test-sites/dice-roll/main.js
@@ -60,13 +60,13 @@ function rollBlackDice() {
     x = blackArray[(Math.floor(Math.random() * 6))];
     y = blackArray[(Math.floor(Math.random() * 6))];
     z = whiteArray[(Math.floor(Math.random() * 6))];
-    b = Math.floor(Math.random() * 13);
+    b = Math.floor(Math.random() * 20);
     document.getElementById("roll-1-div").style.visibility = "visible";
     document.getElementById("roll-2-div").style.visibility = "visible";
     document.getElementById("roll-1-div").style.backgroundImage = x;
     document.getElementById("roll-2-div").style.backgroundImage = y;
     document.getElementById("roll-3-div").style.backgroundImage = z;
-    if (b == 6) {
+    if (b == 9 && document.getElementById("bonus-checkbox").checked) {
         document.getElementById("bonus").style.visibility="visible";
         bonusSound.play();
     };
@@ -77,13 +77,13 @@ function rollRedDice() {
     x = redArray[(Math.floor(Math.random() * 6))];
     y = redArray[(Math.floor(Math.random() * 6))];
     z = whiteArray[(Math.floor(Math.random() * 6))];
-    b = Math.floor(Math.random() * 13);
+    b = Math.floor(Math.random() * 20);
     document.getElementById("roll-1-div").style.visibility = "visible";
     document.getElementById("roll-2-div").style.visibility = "visible";
     document.getElementById("roll-1-div").style.backgroundImage = x;
     document.getElementById("roll-2-div").style.backgroundImage = y;
     document.getElementById("roll-3-div").style.backgroundImage = z;
-    if (b == 6) {
+    if (b == 9 && document.getElementById("bonus-checkbox").checked) {
         document.getElementById("bonus").style.visibility="visible";
         bonusSound.play();
     };
@@ -94,13 +94,13 @@ function rollGreenDice() {
     x = greenArray[(Math.floor(Math.random() * 6))];
     y = greenArray[(Math.floor(Math.random() * 6))];
     z = whiteArray[(Math.floor(Math.random() * 6))];
-    b = Math.floor(Math.random() * 13);
+    b = Math.floor(Math.random() * 20);
     document.getElementById("roll-1-div").style.visibility = "visible";
     document.getElementById("roll-2-div").style.visibility = "visible";
     document.getElementById("roll-1-div").style.backgroundImage = x;
     document.getElementById("roll-2-div").style.backgroundImage = y;
     document.getElementById("roll-3-div").style.backgroundImage = z;
-    if (b == 6) {
+    if (b == 9 && document.getElementById("bonus-checkbox").checked) {
         document.getElementById("bonus").style.visibility="visible";
         bonusSound.play();
     };
@@ -111,13 +111,13 @@ function rollBlueDice() {
     x = blueArray[(Math.floor(Math.random() * 6))];
     y = blueArray[(Math.floor(Math.random() * 6))];
     z = whiteArray[(Math.floor(Math.random() * 6))];
-    b = Math.floor(Math.random() * 13);
+    b = Math.floor(Math.random() * 20);
     document.getElementById("roll-1-div").style.visibility = "visible";
     document.getElementById("roll-2-div").style.visibility = "visible";
     document.getElementById("roll-1-div").style.backgroundImage = x;
     document.getElementById("roll-2-div").style.backgroundImage = y;
     document.getElementById("roll-3-div").style.backgroundImage = z;
-    if (b == 6) {
+    if (b == 9 && document.getElementById("bonus-checkbox").checked) {
         document.getElementById("bonus").style.visibility="visible";
         bonusSound.play();
     };
@@ -158,78 +158,44 @@ function bonusButtonDisappear() {
     document.getElementById("bonus").style.visibility="hidden";
 };
 
-function tile1() {
-    alert("more than 1 of each building type allowed per city");
-};
-
-function tile2() {
-    alert("1 worker earned for each mine at end of each phase in addition to the silverling");
-};
-
-function tile3() {
-    alert("2 silverlings per goods sale, not just 1");
-};
-
-function tile4() {
-    alert("1 worker earned per goods sale in addition to the silverling(s)");
-};
-
-function tile5() {
-    alert("receive goods from 2 neighboring depots (not just 1) when ship placed");
-};
-
-function tile6() {
-    alert("silverlings may be used to buy tiles from any depot, not just the black depot");
-};
-
-function tile7() {
-    alert("if you place an animal tile when knowledge tile #7 is on estate, add 1 extra point for the animal tile itself that you place plus 1 extra point for any other animal tiles of the same animal type on the same pasture");
-};
-
-function tile8() {
-    alert("worker tiles can adjust dice roll by up to +2 or -2");
-};
-
-function tile9() {
-    alert("any dice result may be adjusted +1 or -1 to place a building");
-};
-
-function tile10() {
-    alert("any dice result may be adjusted +1 or -1 to place a ship or animal tile");
-};
-
-function tile11() {
-    alert("any dice result may be adjusted +1 or -1 to place a castle, knowledge tile, or mine");
-};
-
-function tile12() {
-    alert("any dice result may be adjusted +1 or -1 to acquire any new tile");
-};
-
-function tile13() {
-    alert("when a dice is traded for worker tiles, a silverling is also earned");
-};
-
-function tile14() {
-    alert("4 worker tiles traded for a dice, not just 2");
-};
-
-function tile15() {
-    alert("at end of game: 3 points for every goods type sold");
-};
-
-function tile16_23() {
-    alert("at end of game: 4 points for each corresponding building on the estate");
-};
-
-function tile24() {
-    alert("at end of game: 4 points for each animal type on estate");
-};
-
-function tile25() {
-    alert("at end of game: 1 point for every goods tile sold");
-};
-
-function tile26() {
-    alert("at end of game: 2 points for each bonus tile");
+function knowledgeTile(x) {
+    if (x == 1) {
+        alert("more than 1 of each building type allowed per city");
+    } else if (x == 2) {
+        alert("1 worker earned for each mine at end of each phase in addition to the silverling");
+    } else if (x == 3) {
+        alert("2 silverlings per goods sale, not just 1");
+    } else if (x == 4) {
+        alert("1 worker earned per goods sale in addition to the silverling(s)");
+    } else if (x == 5) {
+        alert("receive goods from 2 neighboring depots (not just 1) when ship placed");
+    } else if (x == 6) {
+        alert("silverlings may be used to buy tiles from any depot, not just the black depot");
+    } else if (x == 7) {
+        alert("if you place an animal tile when knowledge tile #7 is on estate, add 1 extra point for the animal tile itself that you place plus 1 extra point for any other animal tiles of the same animal type on the same pasture");
+    } else if (x == 8) {
+        alert("worker tiles can adjust dice roll by up to +2 or -2");
+    } else if (x == 9) {
+        alert("any dice result may be adjusted +1 or -1 to place a building");
+    } else if (x == 10) {
+        alert("any dice result may be adjusted +1 or -1 to place a ship or animal tile");
+    } else if (x == 11) {
+        alert("any dice result may be adjusted +1 or -1 to place a castle, knowledge tile, or mine");
+    } else if (x == 12) {
+        alert("any dice result may be adjusted +1 or -1 to acquire any new tile");
+    } else if (x == 13) {
+        alert("when a dice is traded for worker tiles, a silverling is also earned");
+    } else if (x == 14) {
+        alert("4 worker tiles traded for a dice, not just 2");
+    } else if (x == 15) {
+        alert("at end of game: 3 points for every goods type sold");
+    } else if (x == 16) {
+        alert("at end of game: 4 points for each corresponding building on the estate");
+    } else if (x == 24) {
+        alert("at end of game: 4 points for each animal type on estate");
+    } else if (x == 25) {
+        alert("at end of game: 1 point for every goods tile sold");
+    } else {
+        alert("at end of game: 2 points for each bonus tile");
+    }
 };
