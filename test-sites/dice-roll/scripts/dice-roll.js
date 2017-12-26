5b4b1da524a2b09218f19223d929927694153520
jhmulkey <32462749+jhmulkey@users.noreply.github.com>
2017-12-25 23:16:22 -0500

    edit

--- test-sites/dice-roll/scripts/dice-roll.js
+++ test-sites/dice-roll/scripts/dice-roll.js
@@ -4,74 +4,22 @@ diceRoll.src = "./audio/dice-roll.mp3";
 var bonusSound = new Audio();
 bonusSound.src = "./audio/bonus-sound.mp3";
 
-players = 0;
-    
-r = 0;
-
-p = 0;
-
-whiteArray = [
-    "url(images/white-dice-1.png)",
-    "url(images/white-dice-2.png)",
-    "url(images/white-dice-3.png)",
-    "url(images/white-dice-4.png)",
-    "url(images/white-dice-5.png)",
-    "url(images/white-dice-6.png)"
-];
-
-blackArray = [
-    "url(images/black-dice-1.png)",
-    "url(images/black-dice-2.png)",
-    "url(images/black-dice-3.png)",
-    "url(images/black-dice-4.png)",
-    "url(images/black-dice-5.png)",
-    "url(images/black-dice-6.png)"
-];
-
-redArray = [
-    "url(images/red-dice-1.png)",
-    "url(images/red-dice-2.png)",
-    "url(images/red-dice-3.png)",
-    "url(images/red-dice-4.png)",
-    "url(images/red-dice-5.png)",
-    "url(images/red-dice-6.png)"
-];
-
-greenArray = [
-    "url(images/green-dice-1.png)",
-    "url(images/green-dice-2.png)",
-    "url(images/green-dice-3.png)",
-    "url(images/green-dice-4.png)",
-    "url(images/green-dice-5.png)",
-    "url(images/green-dice-6.png)"
-];
-
-blueArray = [
-    "url(images/blue-dice-1.png)",
-    "url(images/blue-dice-2.png)",
-    "url(images/blue-dice-3.png)",
-    "url(images/blue-dice-4.png)",
-    "url(images/blue-dice-5.png)",
-    "url(images/blue-dice-6.png)"
-];
-
-function rollWhiteDice() {
-    diceRoll.play();
-    x = whiteArray[(Math.floor(Math.random() * 6))];
-    document.getElementById("roll-1-div").style.backgroundImage = x;
-};
+_pl = 0;
+_rd = 0;
+_ph = 0;
+_pts = 0;
 
 function rollDice(color) {
-    if (r < 25) {
+    if (_rd < 25) {
         diceRoll.play();
-        r++;
+        _rd++;
         randomDice(color);
         setPhase();
         setRound();
         if (document.getElementById("bonus-checkbox").checked) {
             randomBonus();
         }
-    } else if (r >= 25) {
+    } else if (_rd >= 25) {
         if (window.confirm("End of Game. Ok to reset?")) {
             location.reload();
         };
@@ -79,38 +27,28 @@ function rollDice(color) {
 };
 
 function setPlayers() {
-    x = prompt("How many players (2-4)?");
-    y = parseInt(x);
-    if (isNaN(y) || y < 2 || y > 4) {
+    x = parseInt(prompt("How many players (2-4)?"));
+    if (isNaN(x) || x < 2 || x > 4) {
         alert("Please enter a number between 2 and 4");
         setPlayers();
     } else {
-        players = y;
-        document.getElementById("player-span").innerHTML = y;
+        _pl = x;
+        document.getElementById("player-span").innerHTML = x;
     };
 };
 
 function randomDice(color) {
-    diceRoll.play();
-    if (color == "black") {
-        x = blackArray[(Math.floor(Math.random() * 6))];
-        y = blackArray[(Math.floor(Math.random() * 6))];
-    } else if (color == "red") {
-        x = redArray[(Math.floor(Math.random() * 6))];
-        y = redArray[(Math.floor(Math.random() * 6))];
-    } else if (color == "green") {
-        x = greenArray[(Math.floor(Math.random() * 6))];
-        y = greenArray[(Math.floor(Math.random() * 6))];
-    } else if (color == "blue") {
-        x = blueArray[(Math.floor(Math.random() * 6))];
-        y = blueArray[(Math.floor(Math.random() * 6))];
-    };
-    z = whiteArray[(Math.floor(Math.random() * 6))];
+    x = Math.floor(Math.random() * 6) + 1;
+    y = Math.floor(Math.random() * 6) + 1;
+    z = Math.floor(Math.random() * 6) + 1;
+    dice1 = "url(images/" + color + "-dice-" + x + ".png)";
+    dice2 = "url(images/" + color + "-dice-" + y + ".png)";
+    dice3 = "url(images/white-dice-" + z + ".png)";
     document.getElementById("roll-1-div").style.visibility = "visible";
     document.getElementById("roll-2-div").style.visibility = "visible";
-    document.getElementById("roll-1-div").style.backgroundImage = x;
-    document.getElementById("roll-2-div").style.backgroundImage = y;
-    document.getElementById("roll-3-div").style.backgroundImage = z;
+    document.getElementById("roll-1-div").style.backgroundImage = dice1;
+    document.getElementById("roll-2-div").style.backgroundImage = dice2;
+    document.getElementById("roll-3-div").style.backgroundImage = dice3;
 };
 
 function randomBonus() {
@@ -122,29 +60,19 @@ function randomBonus() {
 };
 
 function setPhase() {
-    if (r == 6) {
-        document.getElementById("phase-span").innerHTML = "B";
-    } else if (r == 11) {
-        document.getElementById("phase-span").innerHTML = "C";
-    } else if (r == 16) {
-        document.getElementById("phase-span").innerHTML = "D";
-    } else if (r == 21) {
-        document.getElementById("phase-span").innerHTML = "E";
+    if (_rd == 6) {
+        document.getElementById("phase-span").innerHTML = "B"; _ph = 1;
+    } else if (_rd == 11) {
+        document.getElementById("phase-span").innerHTML = "C"; _ph = 2;
+    } else if (_rd == 16) {
+        document.getElementById("phase-span").innerHTML = "D"; _ph = 3;
+    } else if (_rd == 21) {
+        document.getElementById("phase-span").innerHTML = "E"; _ph = 4;
     };
 };
 
 function setRound() {
-    if (r == 1 || r == 6 || r == 11 || r == 16 || r == 21) {
-        document.getElementById("round-span").innerHTML = 1;
-    } else if (r == 2 || r == 7 || r == 12 || r == 17 || r == 22) {
-        document.getElementById("round-span").innerHTML = 2;
-    } else if (r == 3 || r == 8 || r == 13 || r == 18 || r == 23) {
-        document.getElementById("round-span").innerHTML = 3;
-    } else if (r == 4 || r == 9 || r == 14 || r == 19 || r == 24) {
-        document.getElementById("round-span").innerHTML = 4;
-    } else if (r == 5 || r == 10 || r == 15 || r == 20 || r == 25) {
-        document.getElementById("round-span").innerHTML = 5;
-    };
+    document.getElementById("round-span").innerHTML = _rd - (5 * _ph);
 };
 
 function hide1() {
@@ -172,237 +100,188 @@ function whiteDiceVisible() {
 };
 
 function setPoints() {
-    x = prompt("Set points to:");
-    y = parseInt(x);
-    if (isNaN(y)) {
+    x = parseInt(prompt("Set points to:"));
+    if (isNaN(x)) {
         alert("Please enter a number");
     } else {
-        p = y;
-        document.getElementById("total-points").innerHTML = p;
-        alert("+" + y + ". " + "Your new point total is " + p);
+        _pts = x;
+        document.getElementById("total-points").innerHTML = _pts;
+        alert("Points set to " + _pts);
         window.scrollTo(0,0);
     };
 };
 
 function resetPoints() {
     if (window.confirm("Are you sure you want to reset points?")) {
-        p = 0;
-        document.getElementById("total-points").innerHTML = p;
+        _pts = 0;
+        document.getElementById("total-points").innerHTML = _pts;
         window.scrollTo(0,0);
     };
 };
 
 function adjustPoints() {
-    x = prompt("Adjust points by:");
-    y = parseInt(x);
-    if (isNaN(y)) {
+    x = parseInt(prompt("Adjust points by:"));
+    if (isNaN(x)) {
         alert("Please enter a number");
     } else {
-        p += y;
-        document.getElementById("total-points").innerHTML = p;
-        if (y >= 0) {
-            alert("+" + y + ". " + "Your new point total is " + p);
+        _pts += x;
+        document.getElementById("total-points").innerHTML = _pts;
+        if (x >= 0) {
+            alert("+" + x + ". " + "Your new point total is " + _pts);
         } else {
-            alert(y + ". " + "Your new point total is " + p);
-            window.scrollTo(0,0);
+            alert(x + ". " + "Your new point total is " + _pts);
         };
+        window.scrollTo(0,0);
     };
 };
 
 function addPoints() {
-    x = prompt("Points earned:");
-    y = parseInt(x);
-    if (isNaN(y)) {
+    x = parseInt(prompt("Points earned:"));
+    if (isNaN(x)) {
         alert("Please enter a number");
     } else {
-        p += y;
-        document.getElementById("total-points").innerHTML = p;
-        alert("+" + y + ". " + "Your new point total is " + p);
+        _pts += x;
+        document.getElementById("total-points").innerHTML = _pts;
+        alert("+" + x + ". " + "Your new point total is " + _pts);
         window.scrollTo(0,0);
     };
 };
 
 function tapPoints(x) {
-    p += x;
-    document.getElementById("total-points").innerHTML = p;
-    alert("+" + x + ". " + "Your new point total is " + p);
+    _pts += x;
+    document.getElementById("total-points").innerHTML = _pts;
+    alert("+" + x + ". " + "Your new point total is " + _pts);
     window.scrollTo(0,0);
 };
 
 function regionPoints(x) {
-    a = 11; b = 13; c = 16; d = 20; e = 25; f = 31; g = 38; h = 46;
-    if (r > 5 && r < 11) {
-        a = 9; b = 11; c = 14; d = 18; e = 23; f = 29; g = 36; h = 44;
-    } else if (r > 10 && r < 16) {
-        a = 7; b = 9; c = 12; d = 16; e = 21; f = 27; g = 34; h = 42;
-    } else if (r > 15 && r < 21) {
-        a = 5; b = 7; c = 10; d = 14; e = 19; f = 25; g = 32; h = 40;
-    } else if (r > 20) {
-        a = 3; b = 5; c = 8; d = 12; e = 17; f = 23; g = 30; h = 38;
-    };
-    if (x == 1) {
-        p += a; added = a; 
-    } else if (x == 2) {
-        p += b; added = b; 
-    } else if (x == 3) {
-        p += c; added = c; 
-    } else if (x == 4) {
-        p += d; added = d; 
-    } else if (x == 5) {
-        p += e; added = e; 
-    } else if (x == 6) {
-        p += f; added = f; 
-    } else if (x == 7) {
-        p += g; added = g; 
-    } else if (x == 8) {
-        p += h; added = h; 
-    };
-    document.getElementById("total-points").innerHTML = p;
-    alert("+" + added + ". " + "Your new point total is " + p);
+    points = [11, 13, 16, 20, 25, 31, 38, 46];
+    added = points[x] - (_ph * 2); _pts += added;
+    document.getElementById("total-points").innerHTML = _pts;
+    alert("+" + added + ". " + "Your new point total is " + _pts);
     window.scrollTo(0,0);
 };
 
 function sellGoods() {
-    if (players < 2 || players > 4) {
+    if (_pl < 2 || _pl > 4) {
         setPlayers();
         sellGoods();
     } else {
-        x = prompt("How many goods sold?");
-        y = parseInt(x);
-        if (isNaN(y) || y < 0 || y > 10) {
+        x = parseInt(prompt("How many goods sold?"));
+        if (isNaN(x) || x < 0 || x > 10) {
         alert("Please enter a number between 1 and 10");
         } else {
-            if (players == 2) {
-                p += y * 2;
-                document.getElementById("total-points").innerHTML = p;
-                alert("+" + y * 2 + ". " + "Your new point total is " + p);
-                window.scrollTo(0,0);
-            } else if (players == 3) {
-                p += y * 3;
-                document.getElementById("total-points").innerHTML = p;
-                alert("+" + y * 3 + ". " + "Your new point total is " + p);
-                window.scrollTo(0,0);
-            } else if (players == 4) {
-                p += y * 4;
-                document.getElementById("total-points").innerHTML = p;
-                alert("+" + y * 4 + ". " + "Your new point total is " + p);
-                window.scrollTo(0,0);
-            };
+            _pts += x * _pl;
+            document.getElementById("total-points").innerHTML = _pts;
+            alert("+" + (x * _pl) + ". " + "Your new point total is " + _pts);
+            window.scrollTo(0,0);
         };
     };
 };
 
 function bonusTile(x) {
-    if (players < 2 || players > 4) {
+    if (_pl < 2 || _pl > 4) {
         setPlayers();
     } else {
         if (x == 1) {
-            p += (players + 3);
-            document.getElementById("total-points").innerHTML = p;
-            alert("+" + (players + 3) + ". " + "Your new point total is " + p);
+            _pts += (_pl + 3);
+            document.getElementById("total-points").innerHTML = _pts;
+            alert("+" + (_pl + 3) + ". " + "Your new point total is " + _pts);
             window.scrollTo(0,0);
         } else if (x == 2) {
-            p += players;
-            document.getElementById("total-points").innerHTML = p;
-            alert("+" + players + ". " + "Your new point total is " + p);
+            _pts += _pl;
+            document.getElementById("total-points").innerHTML = _pts;
+            alert("+" + _pl + ". " + "Your new point total is " + _pts);
             window.scrollTo(0,0);
         };
     };
 };
 
 function unsoldGoods() {
-    x = prompt("How many unsold goods tiles remain?");
-    y = parseInt(x);
-    if (isNaN(y)) {
+    x = parseInt(prompt("How many unsold goods tiles remain?"));
+    if (isNaN(x)) {
         alert("Please enter a number");
     } else {
-        p += y;
-        document.getElementById("total-points").innerHTML = p;
-        alert("+" + y + ". " + "Your new point total is " + p);
+        _pts += x;
+        document.getElementById("total-points").innerHTML = _pts;
+        alert("+" + x + ". " + "Your new point total is " + _pts);
     };
 };
 
 function remSilver() {
-    x = prompt("How many silverlings remain?");
-    y = parseInt(x);
-    if (isNaN(y)) {
+    x = parseInt(prompt("How many silverlings remain?"));
+    if (isNaN(x)) {
         alert("Please enter a number");
     } else {
-        p += y;
-        document.getElementById("total-points").innerHTML = p;
-        alert("+" + y + ". " + "Your new point total is " + p);
+        _pts += x;
+        document.getElementById("total-points").innerHTML = _pts;
+        alert("+" + x + ". " + "Your new point total is " + _pts);
     };
 };
 
 function remWorker() {
-    x = prompt("How many workers remain?");
-    y = parseInt(x);
-    if (isNaN(y)) {
+    x = parseInt(prompt("How many workers remain?"));
+    if (isNaN(x)) {
         alert("Please enter a number");
     } else {
-        p += Math.floor(y / 2);
-        document.getElementById("total-points").innerHTML = p;
-        alert("+" + Math.floor(y / 2) + ". " + "Your new point total is " + p);
+        _pts += Math.floor(x / 2);
+        document.getElementById("total-points").innerHTML = _pts;
+        alert("+" + Math.floor(x / 2) + ". " + "Your new point total is " + _pts);
     };
 };
 
 function know15() {
-    x = prompt("How goods types sold?");
-    y = parseInt(x);
-    if (isNaN(y)) {
+    x = parseInt(prompt("How goods types sold?"));
+    if (isNaN(x)) {
         alert("Please enter a number");
     } else {
-        p += (y * 3);
-        document.getElementById("total-points").innerHTML = p;
-        alert("+" + (y * 3) + ". " + "Your new point total is " + p);
+        _pts += (x * 3);
+        document.getElementById("total-points").innerHTML = _pts;
+        alert("+" + (x * 3) + ". " + "Your new point total is " + _pts);
     };
 };
 
 function know1623() {
-    x = prompt("How many eligible buildings?");
-    y = parseInt(x);
-    if (isNaN(y)) {
+    x = parseInt(prompt("How many eligible buildings?"));
+    if (isNaN(x)) {
         alert("Please enter a number");
     } else {
-        p += (y * 4);
-        document.getElementById("total-points").innerHTML = p;
-        alert("+" + (y * 4) + ". " + "Your new point total is " + p);
+        _pts += (x * 4);
+        document.getElementById("total-points").innerHTML = _pts;
+        alert("+" + (x * 4) + ". " + "Your new point total is " + _pts);
     };
 };
 
 function know24() {
-    x = prompt("How many animal types on estate?");
-    y = parseInt(x);
-    if (isNaN(y)) {
+    x = parseInt(prompt("How many animal types on estate?"));
+    if (isNaN(x)) {
         alert("Please enter a number");
     } else {
-        p += (y * 4);
-        document.getElementById("total-points").innerHTML = p;
-        alert("+" + (y * 4) + ". " + "Your new point total is " + p);
+        _pts += (x * 4);
+        document.getElementById("total-points").innerHTML = _pts;
+        alert("+" + (x * 4) + ". " + "Your new point total is " + _pts);
     };
 };
 
 function know25() {
-    x = prompt("How many goods tiles sold?");
-    y = parseInt(x);
-    if (isNaN(y)) {
+    x = parseInt(prompt("How many goods tiles sold?"));
+    if (isNaN(x)) {
         alert("Please enter a number");
     } else {
-        p += y;
-        document.getElementById("total-points").innerHTML = p;
-        alert("+" + y + ". " + "Your new point total is " + p);
+        _pts += x;
+        document.getElementById("total-points").innerHTML = _pts;
+        alert("+" + x + ". " + "Your new point total is " + _pts);
     };
 };
 
 function know26() {
-    x = prompt("How many bonus tiles?");
-    y = parseInt(x);
-    if (isNaN(y)) {
+    x = parseInt(prompt("How many bonus tiles?"));
+    if (isNaN(x)) {
         alert("Please enter a number");
     } else {
-        p += (y * 2);
-        document.getElementById("total-points").innerHTML = p;
-        alert("+" + (y * 2) + ". " + "Your new point total is " + p);
+        _pts += (x * 2);
+        document.getElementById("total-points").innerHTML = _pts;
+        alert("+" + (x * 2) + ". " + "Your new point total is " + _pts);
     };
 };
 
--- test-sites/dice-roll/scripts/index.js
+++ test-sites/dice-roll/scripts/index.js
@@ -1,7 +1,7 @@
 var diceRoll = new Audio();
 diceRoll.src = "./audio/dice-roll.mp3";
 
-whiteArray = [
+dice = [
     "url(images/white-dice-1.png)",
     "url(images/white-dice-2.png)",
     "url(images/white-dice-3.png)",
@@ -10,70 +10,39 @@ whiteArray = [
     "url(images/white-dice-6.png)"
 ];
 
-boards1_9 = [1,2,3,4,5,6,7,8,9];
+boards = [
+    [1,2,3,4,5,6,7,8,9],
+    ["10a","10b","10c","10d","10e","10f","10g","10h"],
+    ["13a","13b","13c","13d","13e","13f","13g","13h"],
+    [1,2,3,4,5,6,7,8,9,"10a","10b","10c","10d","10e","10f","10g","10h","13a","13b","13c","13d","13e","13f","13g","13h"]
+]
 
-boards10a_h = ["10a","10b","10c","10d","10e","10f","10g","10h"];
-
-boards13a_h = ["13a","13b","13c","13d","13e","13f","13g","13h"];
-
-boards1_13h = [1,2,3,4,5,6,7,8,9,"10a","10b","10c","10d","10e","10f","10g","10h","13a","13b","13c","13d","13e","13f","13g","13h"];
-
-function chooseBoard() {
+function chooseBoardGroup() {
     document.getElementById("board-select-div").style.backgroundImage = "none";
     if (document.getElementById("B1-9").checked) {
-        chooseBoards1_9();
+        chooseBoards(0);
     } else if (document.getElementById("B10a-h").checked) {
-        chooseBoards10a_h();
+        chooseBoards(1);
     } else if (document.getElementById("B13a-h").checked) {
-        chooseBoards13a_h();
+        chooseBoards(2);
     } else {
-        chooseBoards1_13h();
+        chooseBoards(3);
     };
 };
 
-function chooseBoards1_9() {
-    if (boards1_9.length == 0) {
+function chooseBoards(x) {
+    if (boards[x].length == 0) {
         location.reload();
     } else {
-        x = Math.floor(Math.random() * (boards1_9.length - 1));
-        document.getElementById("board-select-p").innerHTML = boards1_9[x];
-        boards1_9.splice(x,1);
-    };
-};
-
-function chooseBoards10a_h() {
-    if (boards10a_h.length == 0) {
-        location.reload();
-    } else {
-        x = Math.floor(Math.random() * (boards10a_h.length - 1));
-        document.getElementById("board-select-p").innerHTML = boards10a_h[x];
-        boards10a_h.splice(x,1);
-    };
-};
-
-function chooseBoards13a_h() {
-    if (boards13a_h.length == 0) {
-        location.reload();
-    } else {
-        x = Math.floor(Math.random() * (boards13a_h.length - 1));
-        document.getElementById("board-select-p").innerHTML = boards13a_h[x];
-        boards13a_h.splice(x,1);
-    };
-};
-
-function chooseBoards1_13h() {
-    if (boards1_13h.length == 0) {
-        location.reload();
-    } else {
-        x = Math.floor(Math.random() * (boards1_13h.length - 1));
-        document.getElementById("board-select-p").innerHTML = boards1_13h[x];
-        boards1_13h.splice(x,1);
+        y = Math.floor(Math.random() * (boards[x].length - 1));
+        document.getElementById("board-select-p").innerHTML = boards[x][y];
+        boards[x].splice(y,1);
     };
 };
 
 function rollWhiteDice() {
     diceRoll.play();
     document.getElementById("starting-player-roll-div").style.outline = "none";
-    x = whiteArray[(Math.floor(Math.random() * 6))];
+    x = dice[(Math.floor(Math.random() * 6))];
     document.getElementById("starting-player-roll-div").style.backgroundImage = x;
 };
