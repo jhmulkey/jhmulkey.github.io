3753cd50f0fea5afe40792f5f7c3bc2d9ee79358
jhmulkey <32462749+jhmulkey@users.noreply.github.com>
2017-12-28 22:24:19 -0500

    edit

--- test-sites/dice-roll/scripts/index.js
+++ test-sites/dice-roll/scripts/index.js
@@ -1,34 +1,54 @@
 var rollSound = new Audio();
 rollSound.src = "./audio/roll-sound.mp3";
 
+var _boardsIndex = 0;
+var _randomIndex = 0;
+
 var _boards = [
     [1,2,3,4,5,6,7,8,9],
     ["10a","10b","10c","10d","10e","10f","10g","10h"],
     ["13a","13b","13c","13d","13e","13f","13g","13h"], [1,2,3,4,5,6,7,8,9,"10a","10b","10c","10d","10e","10f","10g","10h","13a","13b","13c","13d","13e","13f","13g","13h"]
 ];
 
-var _boardsIndex = 0;
+var _boardsReset = [
+    [1,2,3,4,5,6,7,8,9],
+    ["10a","10b","10c","10d","10e","10f","10g","10h"],
+    ["13a","13b","13c","13d","13e","13f","13g","13h"], [1,2,3,4,5,6,7,8,9,"10a","10b","10c","10d","10e","10f","10g","10h","13a","13b","13c","13d","13e","13f","13g","13h"]
+];
 
 function setBoardIndex(x) {
     _boardsIndex = x;
+    document.getElementById("select-board-group-pop").style.display = "none";
+    document.getElementById("select-board-pop").style.display = "block";
 };
 
 function chooseBoards(_boardsIndex) {
     if (_boards[_boardsIndex].length == 0) {
-        location.reload();
+        for (i = 0; i < _boardsReset[_boardsIndex].length; i++) {
+            _boards[_boardsIndex].push(_boardsReset[_boardsIndex][i]);
+        };
+        chooseBoards(_boardsIndex);
     } else {
-        document.getElementById("board-select-div").style.color = "black";
-        document.getElementById("board-select-div").style.fontSize = "60px";
-        document.getElementById("board-select-div").style.paddingTop = "12px";
-        var y = Math.floor(Math.random() * (_boards[_boardsIndex].length - 1));
-        document.getElementById("board-select-div").innerHTML = _boards[_boardsIndex][y];
-        _boards[_boardsIndex].splice(y,1);
+        var x = Math.floor(Math.random() * (_boards[_boardsIndex].length));
+        document.getElementById("tap-to-select").innerHTML = _boards[_boardsIndex][x];
+        _boards[_boardsIndex].splice(x,1);
     };
 };
 
 function rollWhiteDice() {
     rollSound.play();
     var x = (Math.floor(Math.random() * 6)) + 1;
-    document.getElementById("roll-white-dice-div").innerHTML = "";
-    document.getElementById("roll-white-dice-div").style.backgroundImage = "url(images/white-dice-" + x + ".png)";
+    document.getElementById("tap-to-roll").style.backgroundImage = "url(images/white-dice-" + x + ".png)";
 };
+
+function titleScreen() {
+    document.getElementById("title-screen").style.display = "none";
+    document.getElementById("select-board-group-pop").style.display = "block";
+}
+
+function whiteDicePop() {
+    document.getElementById("select-board-pop").style.display = "none";
+    document.getElementById("white-dice-pop").style.display = "block";
+}
+
+setTimeout(titleScreen, 2000);
