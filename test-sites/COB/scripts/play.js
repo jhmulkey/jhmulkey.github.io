68ed444ab746b809b0a44ba56ca71ebb7ef65340
jhmulkey <32462749+jhmulkey@users.noreply.github.com>
2018-01-06 11:40:43 -0500

    edit

--- test-sites/COB/scripts/play.js
+++ test-sites/COB/scripts/play.js
@@ -60,17 +60,6 @@ function setPlayers(x) {
     pop("pos","block");
 };
 
-function initializeWorkers(x) {
-    if (x > _pl) {
-        alert("Cannot be player " + x + " in a " + _pl + " player game")
-    } else {
-        _workers = x;
-        document.getElementById("worker-count").innerHTML = _workers;
-        pop("pos","block");
-        pop("cs","block");
-    };
-};
-
 function setColor(color) {
     _color = color;
     pop("cs","block");
@@ -78,20 +67,20 @@ function setColor(color) {
 };
 
 function rollDice(color) {
+    _rd++;
     if (_rd < 25) {
-        rollSound.play();
-        _rd++;
-        randomDice(color);
-        setPhaseRound();
         if (document.getElementById("bonus-checkbox").checked) {
             randomBonus();
         };
         if (_mines > 0 && (_rd == 6 || _rd == 11 || _rd == 16 || _rd == 21)) {
-            adjustSilver(_mines,4);
+            addSilver(_mines,"mines");
         };
         if (_k.k2 === true && (_rd == 6 || _rd == 11 || _rd == 16 || _rd == 21)) {
-            adjustWorkers(_mines,"mines");
+            addWorkers(_mines,"mines");
         };
+        setPhaseRound();
+        rollSound.play();
+        randomDice(color);
     } else if (_rd >= 25) {
         if (window.confirm("Click OK to cacluate end of game points.")) {
             document.getElementById("latest-activity-span").innerHTML = "calculating end game points";
@@ -168,7 +157,6 @@ function adjustRound(x) {
     document.getElementById("phase-span").innerHTML = _adjPh;
     document.getElementById("round-span").innerHTML = _rd - (5 * _ph);
     activityLog("Phase " + _adjPh,"black","25px","20px","h1");
-    console.log("hello");
     var log = "round adjusted to Phase " + _adjPh + " Round " + x;
     latestActivity("red",log);
     activityLog(log,"red");
@@ -185,47 +173,62 @@ function randomBonus() {
     };
 };
 
-function adjustWorkers(x,name,trade,type) {
-    var added = x;
-    if (trade) {
-        if (_k.k13 === true && _k.k14 === false) {
-            _workers += x;
-            adjustSilver(1,5);
-        } else if (_k.k14 === true && _k.k13 === false) {
-            _workers += 4;
-            added = 4;
-            pointSound.play();
-        } else if (_k.k13 === true && _k.k14 === true) {
-            _workers += 4;
-            adjustSilver(1,5);
-            added = 4;
-        } else if (_k.k13 === false && _k.k14 === false) {
-            _workers += x;
-            pointSound.play();
-        }
+function setPoints() {
+    var x = prompt("Set points to:");
+    if (x === null) {
+        return;
     } else {
-        if ((_workers + x) < 0 && x < 0) {
-            alert("You don't have enough workers");
-            pop("w","block");
-            return;
-        } else {
-            _workers += x;
-        };
+        var y = parseInt(x);
+    };
+    if (isNaN(y)) {
+        alert("Please enter a number");
+        setPoints();
+    } else {
+        _pts = y;
+        document.getElementById("total-points").innerHTML = _pts;
+        var log = "points set to " + y;
+        latestActivity("red",log);
+        activityLog(log,"red");
         pointSound.play();
+        pop("mm","block");
+        window.scrollTo(0,0);
     };
-    document.getElementById("worker-count").innerHTML = _workers;
-    if (name == "used") {
-        var log = Math.abs(added) + " workers used";
-        pop("w","block");
-    } else {
-        var log = added + " workers for " + name;
-    };
+};
+
+function tapPoints(x,name,type) {
+    _pts += x;
+    document.getElementById("total-points").innerHTML = _pts;
+    var log = x + " points for " + name;
     latestActivity("black",log);
     activityLog(log);
+    pointSound.play();
     if (type == "building") {
         pop("b","flex");
     };
     window.scrollTo(0,0);
+};
+
+function regionPoints(x) {
+    var points = [11, 13, 16, 20, 25, 31, 38, 46];
+    var added = points[x] - (_ph * 2); 
+    _pts += added;
+    document.getElementById("total-points").innerHTML = _pts;
+    var log = added + " points for region size " + (x + 1) + " in Phase " + document.getElementById("phase-span").innerHTML;
+    latestActivity("black",log);
+    activityLog(log);
+    pointSound.play();
+    pop('cr','block');
+};
+
+function initializeWorkers(x) {
+    if (x > _pl) {
+        alert("Cannot be player " + x + " in a " + _pl + " player game")
+    } else {
+        _workers = x;
+        document.getElementById("worker-count").innerHTML = _workers;
+        pop("pos","block");
+        pop("cs","block");
+    };
 };
 
 function setWorkers() {
@@ -248,6 +251,98 @@ function setWorkers() {
         pop("w","block");
         window.scrollTo(0,0);
     };
+};
+
+function addWorkers(x,name) {
+    var total = x;
+    
+    if (_k.k14 === true && name == "dice trade") {
+        total = (x * 2)
+        _workers += total;
+    } else {
+        _workers += x;
+    };
+    
+    if (_k.k13 === true && name == "dice trade") {
+        addSilver(1,"dice trade");
+    };
+    
+    document.getElementById("worker-count").innerHTML = _workers;
+    var log = total + " workers for " + name;
+    latestActivity("black",log);
+    activityLog(log);
+    pointSound.play();
+    if (name == "boarding house") {
+        pop("b","flex");
+    };
+    scrollTo(0,0);
+};
+
+function useWorkers(x) {
+    if (_workers + x < 0) {
+        alert("You only have " + _workers + " workers");
+        return;
+    } else {
+        _workers += x;
+    };
+    document.getElementById("worker-count").innerHTML = _workers;
+    var log = Math.abs(x) + " workers used";
+    latestActivity("black",log);
+    activityLog(log);
+    pointSound.play();
+    pop("w","block");
+    scrollTo(0,0);
+};
+
+function setSilver() {
+    var x = prompt("Set silverlings to:");
+    if (x === null) {
+        return;
+    } else {
+        var y = parseInt(x);
+    };
+    if (isNaN(y)) {
+        alert("Please enter a number");
+        setSilver();
+    } else {
+        _silver = y;
+        document.getElementById("silver-count").innerHTML = _silver;
+        var log = "silverlings set to " + y;
+        latestActivity("red",log);
+        activityLog(log,"red");
+        silverSound.play();
+        pop("si","block");
+        window.scrollTo(0,0);
+    };
+};
+
+function addSilver(x,name) {
+    _silver += x;
+    document.getElementById("silver-count").innerHTML = _silver;
+    var log = x + " silverlings for " + name;
+    latestActivity("black",log);
+    activityLog(log);
+    silverSound.play();
+    if (name == "bank") {
+        pop("b","flex");
+    };
+    scrollTo(0,0);
+};
+
+function spendSilver(x) {
+    if (_silver + x < 0) {
+        alert("You only have " + _silver + " silverlings");
+        return;
+    } else {
+        _silver += x;
+    };
+    document.getElementById("silver-count").innerHTML = _silver;
+    var log = Math.abs(x) + " silverlings spent";
+    latestActivity("black",log);
+    activityLog(log);
+    silverSound.play();
+    pop("si","block");
+    scrollTo(0,0);
 };
 
 function setMines(x) {
@@ -274,102 +369,6 @@ function addMines() {
     };
 };
 
-function adjustSilver(x,i,type) {
-    var sources = [
-        "adjustment",
-        "spent",
-        "for goods sale",
-        "for bank",
-        "for mines",
-        "for dice trade",        
-    ];
-    if (i == 0) {
-        var y = prompt("Set silverlings to:");
-        if (y === null) {
-            return;
-        } else {
-            var z = parseInt(y);
-        };
-        if (isNaN(z)) {
-            alert("Please enter a number");
-            return;
-        } else {
-            _silver = z;
-            document.getElementById("silver-count").innerHTML = _silver;
-            var log = "silverlings set to " + z;
-            latestActivity("red",log);
-            activityLog(log,"red");
-            pop("si","block");
-        };
-    } else if (i == 1) {
-        if ((_silver + x) < 0) {
-            alert("You don't have enough silverlings");
-            pop("si","block");
-            return;
-        } else {
-            _silver += x;
-            document.getElementById("silver-count").innerHTML = _silver;
-            var log = Math.abs(x) + " silverlings " + sources[i];
-            latestActivity("black",log);
-            activityLog(log);
-            pop("si","block");
-        };
-    } else {
-        _silver += x;
-        document.getElementById("silver-count").innerHTML = _silver;
-        var log = x + " silverlings " + sources[i];
-        latestActivity("black",log);
-        activityLog(log);
-    };
-    silverSound.play();
-    if (type == "building") {
-        pop("b","flex");
-    };
-    window.scrollTo(0,0);
-}
-
-function hide(x) {
-    if (document.getElementById("roll-" + x + "-div").style.visibility == "visible") {
-        document.getElementById("roll-" + x + "-div").style.visibility = "hidden";
-    } else {
-        document.getElementById("roll-" + x + "-div").style.visibility = "visible";
-    };
-};
-
-function setPoints() {
-    var x = prompt("Set points to:");
-    if (x === null) {
-        return;
-    } else {
-        var y = parseInt(x);
-    };
-    if (isNaN(y)) {
-        alert("Please enter a number");
-        setPoints();
-    } else {
-        _pts = y;
-        document.getElementById("total-points").innerHTML = _pts;
-        var log = "points set to " + y;
-        latestActivity("red",log);
-        activityLog(log,"red");
-        pointSound.play();
-        pop("mm","block");
-        window.scrollTo(0,0);
-    };
-};
-
-function regionPoints(x) {
-    var points = [11, 13, 16, 20, 25, 31, 38, 46];
-    var added = points[x] - (_ph * 2); 
-    _pts += added;
-    document.getElementById("total-points").innerHTML = _pts;
-    var log = added + " points for region size " + (x + 1) + " in Phase " + document.getElementById("phase-span").innerHTML;
-    latestActivity("black",log);
-    activityLog(log);
-    pointSound.play();
-    pop('cr','block');
-};
-
 function addGoods(x) {
     _unsold += x;
     document.getElementById("unsold-span").innerHTML = _unsold;
@@ -388,12 +387,12 @@ function sellGoods(x) {
         _sold += x;
         _pts += x * _pl;
         if (_k.k3 === true) {
-            adjustSilver(2,2);
+            addSilver(2,"goods sale");
         } else {
-            adjustSilver(1,2);
+            addSilver(1,"goods sale");
         };
         if (_k.k4 === true) {
-            adjustWorkers(1, "goods sale");
+            addWorkers(1,"goods sale");
         };
         document.getElementById("unsold-span").innerHTML = _unsold;
         document.getElementById("sold-span").innerHTML = _sold;
@@ -401,6 +400,7 @@ function sellGoods(x) {
         var log = (x * _pl) + " points for sale of " + x + " goods";
         latestActivity("black",log);
         activityLog(log);
+        pointSound.play();
         pop("sg","block");
     };
 };
@@ -415,34 +415,23 @@ function animals(x) {
     pop("a","block");
 };
 
-function tapPoints(x,name,type) {
+function bonusTile(x,size) {
     _pts += x;
+    _bonus++;
     document.getElementById("total-points").innerHTML = _pts;
-    var log = x + " points for " + name;
+    var log = x + " points for " + size + " bonus tile";
     latestActivity("black",log);
     activityLog(log);
     pointSound.play();
-    if (type == "building") {
-        pop("b","flex");
-    };
     window.scrollTo(0,0);
 };
 
-function bonusTile(x) {
-    if (x == "large") {
-        _pts += _pl + 3;
-        var added = _pl + 3;
-    } else if (x == "small") {
-        _pts += _pl;
-        var added = _pl;
+function hide(x) {
+    if (document.getElementById("roll-" + x + "-div").style.visibility == "visible") {
+        document.getElementById("roll-" + x + "-div").style.visibility = "hidden";
+    } else {
+        document.getElementById("roll-" + x + "-div").style.visibility = "visible";
     };
-    _bonus++;
-    document.getElementById("total-points").innerHTML = _pts;
-    var log = added + " points for " + x + " bonus tile";
-    latestActivity("black",log);
-    activityLog(log);
-    pointSound.play();
-    window.scrollTo(0,0);
 };
 
 function activityLog(log,color,size,marginTop,element) {
@@ -450,16 +439,20 @@ function activityLog(log,color,size,marginTop,element) {
         var elementNode = document.createElement(element);
     } else {
         var elementNode = document.createElement("p");
-    }
+    };
+    
     if (color) {
         elementNode.style.color = color;
     };
+    
     if (marginTop) {
         elementNode.style.marginTop = marginTop;
     };
+    
     if (size) {
         elementNode.style.fontSize = size;
-    }
+    };
+    
     elementNode.style.fontWeight = "bold";
     var textNode = document.createTextNode(log);
     elementNode.appendChild(textNode);
@@ -476,37 +469,21 @@ function addKnowledge(i) {
     var numbers = [2, 3, 4, 13, 14, 15, 1623, 24, 25, 26];
     if (_k[values[i]] === false) {
         _k[values[i]] = true;
-        document.getElementById("k" + numbers[i]).style.borderColor = "red";
-        document.getElementById("k" + numbers[i]).style.borderStyle = "dashed";
-        document.getElementById("k" + numbers[i]).style.borderWidth = "5px";
+        document.getElementById("k" + numbers[i]).style.cssText = "border: 5px dashed red";
         var log = "knowledge tile " + numbers[i] + " added";
         latestActivity("black",log);
         activityLog(log);
-        pointSound.play();
     } else {
        if (window.confirm("Knowledge Tile " + numbers[i] + " is already on estate. Remove?")) {
            _k[values[i]] = false;
-           document.getElementById("k" + numbers[i]).style.borderColor = "black";
-           document.getElementById("k" + numbers[i]).style.borderStyle = "solid";
-           document.getElementById("k" + numbers[i]).style.borderWidth = "2px";
+           document.getElementById("k" + numbers[i]).style.cssText = "border: 2px solid black";
            var log = "knowledge tile " + numbers[i] + " removed";
            latestActivity("red",log);
            activityLog(log,"red");
-           pointSound.play();
         };
     };
+    pointSound.play();
     pop('k','flex');
-};
-
-function tradeDiceForWorkers() {
-    if (_k.k14 === true) {
-        adjustWorkers(4);
-    } else {
-        adjustWorkers(2);
-    };
-    if (_k.k13 === true) {
-        adjustSilver(1,5);
-    };
 };
 
 function totalScore() {
