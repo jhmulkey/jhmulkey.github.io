9595d9b05d8e366ebb5d76b7c5403543184cbc77
jhmulkey <32462749+jhmulkey@users.noreply.github.com>
2018-01-25 15:49:30 -0500

    edit

--- test-sites/class-records/main.js
+++ test-sites/class-records/main.js
@@ -5,9 +5,8 @@ var feedbackSound = new Audio();
 feedbackSound.src = "feedback.mp3";
 
 var _ci; //current index
-var _asNum;
-var _mvNum;
-var _attendance = 0;
+var _asNum; // activity sheet number
+var _mvNum; // memory verse number
 
 class Student {
     constructor(first,last) {
@@ -47,6 +46,7 @@ class Student {
             as35: 0,
             as36: 0,
             as37: 0,
+            as38: 0,
             asq3b: 0,
             as41: 0,
             as42: 0,
@@ -83,6 +83,7 @@ class Student {
             mv35: 0,
             mv36: 0,
             mv37: 0,
+            mv38: 0,
             mv41: 0,
             mv42: 0,
             mv43: 0,
@@ -94,60 +95,33 @@ class Student {
     }
 };
 
-var sl = []; // student list
-var Aldrich1 = new Student("Aeralind","Aldrich");
-var Aldrich2 = new Student("Bronwyn","Aldrich");
-var Ballard = new Student("Calvin","Ballard");
-var Brown = new Student("Evan","Brown");
-var Childs = new Student("Darius","Childs");
-var Collins = new Student("Samuel","Collins");
-var Cronin = new Student("Ben","Cronin");
-var Cruice = new Student("Ashton","Cruice");
-var Deming = new Student("Raegan","Deming");
-var Hotchkiss = new Student("Rowan","Hotchkiss");
-var Ice = new Student("Isaac","Ice");
-var Mair = new Student("Bella","Mair");
-var Morris = new Student("Connell","Morris");
-var Neal = new Student("Katie","Neal");
-var Nelson = new Student("Annaliese","Nelson");
-var Noblitt = new Student("Sydney","Noblitt");
-var Norton = new Student("Hailey","Norton");
-var Pahnke = new Student("Jill","Pahnke");
-var Peoples = new Student("Job","Peoples");
-var Pidcock = new Student("Blake","Pidcock");
-var Rogers = new Student("Cameron","Rogers");
-var Schley = new Student("Adalyn","Schley");
-var Snyder = new Student("Liam","Snyder");
-var Walker = new Student("Macie","Walker");
-var Waters = new Student("Olivia","Waters");
-
-function pushData() {
-    sl.push(Aldrich1);
-    sl.push(Aldrich2);
-    sl.push(Ballard);
-    sl.push(Brown);
-    sl.push(Childs);
-    sl.push(Collins);
-    sl.push(Cronin);
-    sl.push(Cruice);
-    sl.push(Deming);
-    sl.push(Hotchkiss);
-    sl.push(Ice);
-    sl.push(Mair);
-    sl.push(Morris);
-    sl.push(Neal);
-    sl.push(Nelson);
-    sl.push(Noblitt);
-    sl.push(Norton);
-    sl.push(Pahnke);
-    sl.push(Peoples);
-    sl.push(Pidcock);
-    sl.push(Rogers);
-    sl.push(Schley);
-    sl.push(Snyder);
-    sl.push(Walker);
-    sl.push(Waters);
-};
+sl = [
+    new Student("Aeralind","Aldrich"),
+    new Student("Bronwyn","Aldrich"),
+    new Student("Calvin","Ballard"),
+    new Student("Evan","Brown"),
+    new Student("Darius","Childs"),
+    new Student("Samuel","Collins"),
+    new Student("Ben","Cronin"),
+    new Student("Ashton","Cruice"),
+    new Student("Raegan","Deming"),
+    new Student("Rowan","Hotchkiss"),
+    new Student("Isaac","Ice"),
+    new Student("Bella","Mair"),
+    new Student("Connell","Morris"),
+    new Student("Katie","Neal"),
+    new Student("Annaliese","Nelson"),
+    new Student("Sydney","Noblitt"),
+    new Student("Hailey","Norton"),
+    new Student("Jill","Pahnke"),
+    new Student("Job","Peoples"),
+    new Student("Blake","Pidcock"),
+    new Student("Cameron","Rogers"),
+    new Student("Adalyn","Schley"),
+    new Student("Liam","Snyder"),
+    new Student("Macie","Walker"),
+    new Student("Olivia","Waters")
+]; //student list
 
 function newStudent() {
     var first = prompt("First Name:");
@@ -314,6 +288,11 @@ function loadStudent(index) {
     document.getElementById("dispName").innerHTML = sl[_ci].fullName;
     document.getElementById("dispInsig").style.backgroundImage = "url(images/"+sl[_ci].rank+".png)";
     document.getElementById("dispPts").innerHTML = sl[_ci].points;
+    if (sl[_ci].attendance === true) {
+        document.getElementById("attendanceButton").style.borderColor = "green";
+    } else {
+        document.getElementById("attendanceButton").style.borderColor = "red";
+    };
     document.getElementById("search").value = "";
     searchNames();
     for (var key in sl[_ci].as) {
@@ -377,23 +356,6 @@ function mvPop(mvNum,index) {
     };
 };
 
-function promotionList(log) {
-    var elementNode = document.createElement("p");
-    var textNode = document.createTextNode(log);
-    elementNode.appendChild(textNode);
-    document.getElementById("promoList").appendChild(elementNode);
-}
-
-function loadPromotees() {
-    document.getElementById("promoList").innerHTML = "";
-    for (i = 0; i < sl.length; i++) {
-        if (sl[i].promoted === true) {
-            promotionList(sl[i].rankName + " " + sl[i].fullName);
-        };
-    };
-    pop("mainMenuPop","promoListPop");
-};
-
 function attendanceList(log) {
     var elementNode = document.createElement("p");
     var textNode = document.createTextNode(log);
@@ -412,6 +374,23 @@ function loadAttendees() {
     };
     document.getElementById("attCount").innerHTML = attCount;
     pop("mainMenuPop","attListPop");
+};
+
+function promotionList(log) {
+    var elementNode = document.createElement("p");
+    var textNode = document.createTextNode(log);
+    elementNode.appendChild(textNode);
+    document.getElementById("promoList").appendChild(elementNode);
+}
+
+function loadPromotees() {
+    document.getElementById("promoList").innerHTML = "";
+    for (i = 0; i < sl.length; i++) {
+        if (sl[i].promoted === true) {
+            promotionList(sl[i].rankName + " " + sl[i].fullName);
+        };
+    };
+    pop("mainMenuPop","promoListPop");
 };
 
 function populateNames() {
@@ -434,16 +413,10 @@ function attendance() {
     if (sl[_ci].attendance === false) {
         sl[_ci].attendance = true;
         document.getElementById("attendanceButton").style.borderColor = "green";
-        setTimeout(function() {
-            document.getElementById("attendanceButton").style.borderColor = "white";
-        },500);
     } else {
         sl[_ci].attendance = false;
         document.getElementById("attendanceButton").style.borderColor = "red";
-        setTimeout(function() {
-            document.getElementById("attendanceButton").style.borderColor = "white";
-        },500);
-    }
+    };
     storeNewData();
     backupNewData();
     feedbackSound.play();
@@ -481,38 +454,38 @@ function resetDrawing() {
 };
     
 var mvText = [
-    "How precious to me are your thoughts, O God! How vast is the sum of them!",
-    "Have this mind among yourselves, which is yours in Christ Jesus,",
-    "who, though he was in the form of God, did not count equality with God a thing to be grasped,",
-    "but emptied himself, by taking the form of a servant, being born in the likeness of men.",
-    "And being found in human form, he humbled himself by becoming obedient to the point of death, even death on a cross.",
-    "Therefore God has highly exalted him and bestowed on him the name that is above every name,",
-    "so that at the name of Jesus every knee should bow, in heaven and on earth and under the earth,",
-    "and every tongue confess that Jesus Christ is Lord, to the glory of God the Father.",
-    "Blessed are the poor in spirit, for theirs is the kingdom of heaven.",
-    "Blessed are those who mourn, for they shall be comforted.",
-    "Blessed are the meek, for they shall inherit the earth.",
-    "Blessed are those who hunger and thirst for righteousness, for they shall be satisfied.",
-    "Blessed are the merciful, for they shall receive mercy.",
-    "Blessed are the pure in heart, for they shall see God.",
-    "Blessed are the peacemakers, for they shall be called sons of God.",
-    "Blessed are those who are persecuted for righteousness' sake, for theirs is the kingdom of heaven.",
-    "Blessed are you when others revile you and persecute you and utter all kinds of evil against you falsely on my account. Rejoice and be glad, for your reward is great in heaven, for so they persecuted the prophets who were before you.",
-    "What then shall we say to these things? If God is for us, who can be against us?",
-    "He who did not spare his own Son but gave him up for us all, how will he not also with him graciously give us all things?",
-    "Who shall bring any charge against God's elect? It is God who justifies.",
-    "Who is to condemn?  Christ Jesus is the one who died—more than that, who was raised—who is at the right hand of God, who indeed is interceding for us.",
-    "Who shall separate us from the love of Christ? Shall tribulation, or distress, or persecution, or famine, or nakedness, or danger, or sword?",
-    "As it is written, 'For your sake we are being killed all the day long; we are regarded as sheep to be slaughtered.'",
-    "No, in all these things we are more than conquerors through him who loved us.",
-    "For I am sure that neither death nor life, nor angels nor rulers, nor things present nor things to come, nor powers, nor height nor depth, nor anything else in all creation, will be able to separate us from the love of God in Christ Jesus our Lord.",
-    "Finally, be strong in the Lord and in the strength of his might. Put on the whole armor of God, that you may be able to stand against the schemes of the devil.",
-    "For we do not wrestle against flesh and blood, but against the rulers, against the authorities, against the cosmic powers over this present darkness, against the spiritual forces of evil in the heavenly places.",
-    "Therefore take up the whole armor of God, that you may be able to withstand in the evil day, and having done all, to stand firm.",
-    "Stand therefore, having fastened on the belt of truth, and having put on the breastplate of righteousness, and, as shoes for your feet, having put on the readiness given by the gospel of peace.",
-    "n all circumstances take up the shield of faith, with which you can extinguish all the flaming darts of the evil one;",
-    "and take the helmet of salvation, and the sword of the Spirit, which is the word of God,",
-    "praying at all times in the Spirit, with all prayer and supplication. To that end, keep alert with all perseverance, making supplication for all the saints,"  
+    "Ps 139:17<br/>How precious to me are your thoughts, O God! How vast is the sum of them!",
+    "Phil 2:5<br/>Have this mind among yourselves, which is yours in Christ Jesus,",
+    "Phil 2:6<br/>who, though he was in the form of God, did not count equality with God a thing to be grasped,",
+    "Phil 2:7<br/>but emptied himself, by taking the form of a servant, being born in the likeness of men.",
+    "Phil 2:8<br/>And being found in human form, he humbled himself by becoming obedient to the point of death, even death on a cross.",
+    "Phil 2:9<br/>Therefore God has highly exalted him and bestowed on him the name that is above every name,",
+    "Phil 2:10<br/>so that at the name of Jesus every knee should bow, in heaven and on earth and under the earth,",
+    "Phil 2:11<br/>and every tongue confess that Jesus Christ is Lord, to the glory of God the Father.",
+    "Mt 5:3<br/>Blessed are the poor in spirit, for theirs is the kingdom of heaven.",
+    "Mt 5:4<br/>Blessed are those who mourn, for they shall be comforted.",
+    "Mt 5:5<br/>Blessed are the meek, for they shall inherit the earth.",
+    "Mt 5:6<br/>Blessed are those who hunger and thirst for righteousness, for they shall be satisfied.",
+    "Mt 5:7<br/>Blessed are the merciful, for they shall receive mercy.",
+    "Mt 5:8<br/>Blessed are the pure in heart, for they shall see God.",
+    "Mt 5:9<br/>Blessed are the peacemakers, for they shall be called sons of God.",
+    "Mt 5:10<br/>Blessed are those who are persecuted for righteousness' sake, for theirs is the kingdom of heaven.",
+    "Mt 5:11-12<br/>Blessed are you when others revile you and persecute you and utter all kinds of evil against you falsely on my account. Rejoice and be glad, for your reward is great in heaven, for so they persecuted the prophets who were before you.",
+    "Rom 8:31<br/>What then shall we say to these things? If God is for us, who can be against us?",
+    "Rom 8:32<br/>He who did not spare his own Son but gave him up for us all, how will he not also with him graciously give us all things?",
+    "Rom 8:33<br/>Who shall bring any charge against God's elect? It is God who justifies.",
+    "Rom 8:34<br/>Who is to condemn?  Christ Jesus is the one who died—more than that, who was raised—who is at the right hand of God, who indeed is interceding for us.",
+    "Rom 8:35<br/>Who shall separate us from the love of Christ? Shall tribulation, or distress, or persecution, or famine, or nakedness, or danger, or sword?",
+    "Rom 8:36<br/>As it is written, 'For your sake we are being killed all the day long; we are regarded as sheep to be slaughtered.'",
+    "Rom 8:37<br/>No, in all these things we are more than conquerors through him who loved us.",
+    "Rom 8:28-39<br/>For I am sure that neither death nor life, nor angels nor rulers, nor things present nor things to come, nor powers, nor height nor depth, nor anything else in all creation, will be able to separate us from the love of God in Christ Jesus our Lord.",
+    "Eph 6:10-11<br/>Finally, be strong in the Lord and in the strength of his might. Put on the whole armor of God, that you may be able to stand against the schemes of the devil.",
+    "Eph 6:12<br/>For we do not wrestle against flesh and blood, but against the rulers, against the authorities, against the cosmic powers over this present darkness, against the spiritual forces of evil in the heavenly places.",
+    "Eph 6:13<br/>Therefore take up the whole armor of God, that you may be able to withstand in the evil day, and having done all, to stand firm.",
+    "Eph 6:14-15<br/>Stand therefore, having fastened on the belt of truth, and having put on the breastplate of righteousness, and, as shoes for your feet, having put on the readiness given by the gospel of peace.",
+    "Eph 6:16<br/>In all circumstances take up the shield of faith, with which you can extinguish all the flaming darts of the evil one;",
+    "Eph 6:17<br/>and take the helmet of salvation, and the sword of the Spirit, which is the word of God,",
+    "Eph 6:18<br/>praying at all times in the Spirit, with all prayer and supplication. To that end, keep alert with all perseverance, making supplication for all the saints," 
 ];
 
 //***ONLOAD FUNCTIONS***//
@@ -527,25 +500,30 @@ function whatToLoad() {
                 sl[i].promoted = false;
             };
         } else {
-            pushData();
             sortStudentList();
             populateNames();
         };
     } else {
         pop("mainPop","wtlPop");
     };
-}; 
+}; /* if no localStorage data exists, bacjup.js will be loaded; if backup.js is not available, original zeroed data will load
+if localStorage data exists, you are given the choice to load it or backup.js */
 
 function loadBackup() {
-    if (confirm("This action may potentially overwrite more current data. Are you sure?")) {
-        sl = JSON.parse(localStorage.getItem("backup"));
-        sortStudentList();
-        populateNames();
-        for (i = 0; i < sl.length; i++) {
-            sl[i].attendance = false;
-            sl[i].promoted = false;
+    if (!JSON.parse(localStorage.getItem("backup"))) {
+        alert("backup.js not available");
+        return;
+    } else {
+        if (confirm("This action may potentially overwrite more current data. Are you sure?")) {
+            sl = JSON.parse(localStorage.getItem("backup"));
+            sortStudentList();
+            populateNames();
+            for (i = 0; i < sl.length; i++) {
+                sl[i].attendance = false;
+                sl[i].promoted = false;
+            };
+            pop("wtlPop","mainPop");
         };
-        pop("wtlPop","mainPop");
     };
 };
 
@@ -570,45 +548,40 @@ whatToLoad();
 
 document.getElementById("search").focus();
 
+//***POSSIBLE FUNCTIONS FOR FUTURE***//
 
-for (i = 0; i < sl.length; i++) {
-    if (sl[i].drawing === true) {
-       console.log(sl[i].fullName);
-    };
-};
 /*
 asCompletion() {
     var completed = 0;
-    for (var key in this.as) {
-        if (this.as[key] > 0) {
+    for (var key in sl[_ci].as) {
+        if (sl[_ci].as[key] > 0) {
             completed++;
         };
     };
-    return (Math.round(((completed / Object.keys(this.as).length) * 100) * 10) / 10) + " %";
+    return (Math.round(((completed / Object.keys(sl[_ci].as).length) * 100) * 10) / 10) + " %";
 }
 
 mvCompletion() {
     var completed = 0;
-    for (var key in this.mv) {
-        if (this.mv[key] > 0) {
+    for (var key in sl[_ci].mv) {
+        if (sl[_ci].mv[key] > 0) {
             completed++;
         };
     };
-    return (Math.round(((completed / Object.keys(this.mv).length) * 100) * 10) / 10) + " %";
+    return (Math.round(((completed / Object.keys(sl[_ci].mv).length) * 100) * 10) / 10) + " %";
 }
 
-allCompletion() {
+function allCompletion() {
     var completed = 0;
-    for (var key in this.as) {
-        if (this.as[key] > 0) {
+    for (var key in sl[_ci].as) {
+        if (sl[_ci].as[key] > 0) {
             completed++;
         };
     };
-    for (var key in this.mv) {
-        if (this.mv[key] > 0) {
+    for (var key in sl[_ci].mv) {
+        if (sl[_ci].mv[key] > 0) {
             completed++;
         };
     };
-    return (Math.round(((completed / (Object.keys(this.as).length + Object.keys(this.mv).length) * 100) * 10)) / 10) + " %";
-}
-*/
+    document.getElementById("allCompletion").innerHTML = (Math.round(((completed / (Object.keys(sl[_ci].as).length + Object.keys(sl[_ci].mv).length) * 100) * 10)) / 10) + "%";
+}*/
