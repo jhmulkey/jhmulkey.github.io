02384eeb35dfdf1e828bcf6b0246b368da7db446
jhmulkey <32462749+jhmulkey@users.noreply.github.com>
2021-12-27 17:59:10 -0500

    edit

---
+++ class/reset-backup.js
@@ -0,0 +1,10 @@
+var _slBackup = [];
+var _amAttBackup = [];
+var _pmAttBackup = [];
+var _teacherNotesBackup = [];
+
+localStorage.setItem("slBackup",JSON.stringify(_slBackup));
+localStorage.setItem("amAttBackup",JSON.stringify(_amAttBackup));
+localStorage.setItem("pmAttBackup",JSON.stringify(_pmAttBackup));
+localStorage.setItem("teacherNotesBackup",JSON.stringify(_teacherNotesBackup));
+
