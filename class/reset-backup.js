75271a2229d512fc3982c2a3358709c41224a15b
jhmulkey <32462749+jhmulkey@users.noreply.github.com>
2022-08-20 11:28:02 -0400

    edit

--- class/reset-backup.js
+++ class/reset-backup.js
@@ -1,9 +1,8 @@
 var _slBackup = [];
-var _amAttBackup = [];
-var _pmAttBackup = [];
+var _attBackup = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]];
 var _teacherNotesBackup = [];
-
-localStorage.setItem("slBackup",JSON.stringify(_slBackup));
-localStorage.setItem("amAttBackup",JSON.stringify(_amAttBackup));
-localStorage.setItem("pmAttBackup",JSON.stringify(_pmAttBackup));
-localStorage.setItem("teacherNotesBackup",JSON.stringify(_teacherNotesBackup));
+var _handouts = [];
+localStorage.setItem('slBackup',JSON.stringify(_slBackup));
+localStorage.setItem('attBackup',JSON.stringify(_attBackup));
+localStorage.setItem('teacherNotesBackup',JSON.stringify(_teacherNotesBackup));
+localStorage.setItem('handoutsBackup',JSON.stringify(_handouts));
