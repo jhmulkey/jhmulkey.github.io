9dd050f8081a4083357661af2ba9aa1c1d92f428
jhmulkey <32462749+jhmulkey@users.noreply.github.com>
2023-03-25 01:41:20 -0400

    edit

--- class/main.js
+++ class/main.js
@@ -929,8 +929,8 @@ function loadAttendees() {
     } else {
         display("allergiesContainer","none");
     }
-    innerHTML("boysListAttP","Boys (" + boys.length + ")");
-    innerHTML("girlsListAttP","Girls (" + girls.length + ")");
+    innerHTML("boysListAttP","BOYS (" + boys.length + ")");
+    innerHTML("girlsListAttP","GIRLS (" + girls.length + ")");
     pop(["mainPop"],["attListPop"]);
 }
 
@@ -1971,7 +1971,7 @@ function refreshStudentPop() {
     } else {
         ptsNeeded = _rankPts[_sl[_ci].rank[0]+1] - _sl[_ci].pts;
     }
-    innerHTML("studentPopPts",_sl[_ci].pts + " | <span style='color: #999'>" + ptsNeeded +"</span>");
+    innerHTML("studentPopPts",_sl[_ci].pts + " | <span style='color: #333'>" + ptsNeeded +"</span>");
     if (_rankNames[_sl[_ci].rank[0]].length > 20) {
         document.getElementById("studentPopRankName").style.fontSize = "15px";
     } else {
